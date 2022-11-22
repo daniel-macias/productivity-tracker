5ce3b3aed8f80d104d3d70118eb2bee029ac75ba
daniel-macias <josedanielmr@gmail.com>
2022-11-21 21:04:27 -0600

    started angular project and created the goal component

---
+++ src/app/components/goal-tracker/goal-tracker.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { GoalTrackerComponent } from './goal-tracker.component';
+
+describe('GoalTrackerComponent', () => {
+  let component: GoalTrackerComponent;
+  let fixture: ComponentFixture<GoalTrackerComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ GoalTrackerComponent ]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(GoalTrackerComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/components/goal-tracker/goal-tracker.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-goal-tracker',
+  templateUrl: './goal-tracker.component.html',
+  styleUrls: ['./goal-tracker.component.scss']
+})
+export class GoalTrackerComponent {
+
+}
